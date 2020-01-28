0da8cb6b7f3de82fca72d40089e57a5c32c22e6f
Riza Pranata <corodotc@gmail.com>
2020-01-28 13:48:17 +0700

    change position link ubah - delete

--- pertemuan10/index.php
+++ pertemuan10/index.php
@@ -17,27 +17,27 @@
    <table border="1" cellpadding="10" cellspacing="0">
       <tr>
          <th>No.</th>
-         <th>Aksi</th>
          <th>Gambar</th>
          <th>NRP</th>
          <th>Nama</th>
          <th>Email</th>
          <th>Jurusan</th>
+         <th>Aksi</th>
       </tr>
 
       <?php $i = 1; ?>
       <?php foreach( $siswa as $row) : ?>
          <tr>
             <td><?= $i; ?></td>
-            <td>
-               <a href="">ubah</a> |
-               <a href="">hapus</a>
-            </td>
-            <td><img src="img/<?= $row["gambar"]; ?>" width="70" height="93" alt="gambar"></td>
+            <td><img src="img/<?= $row["gambar"]; ?>" width="50" height="63" alt="gambar"></td>
             <td><?= $row["nrp"]; ?></td>
             <td><?= $row["nama"]; ?></td>
             <td><?= $row["email"]; ?></td>
             <td><?= $row["jurusan"]; ?></td>
+            <td>
+               <a href="">ubah</a> |
+               <a href="">hapus</a>
+            </td>
          </tr>
          <?php $i++; ?>
       <?php endforeach; ?>
