4987bf7fb997c1fcff20016354d4e54a60b78e5b
Riza Pranata <riza.ari@dkatalis.com>
2020-01-28 22:25:19 +0700

    menambahkan fungsi searching

---
+++ pertemuan12/functions.php
@@ -0,0 +1,70 @@
+<?php
+    // koneksi ke database
+   $conn = mysqli_connect("localhost","root", "", "phpbasic");
+  
+   function query($query){
+       global $conn;
+       $result = mysqli_query($conn, $query);
+       $rows = [];
+       while($row = mysqli_fetch_assoc($result)){
+           $rows[] = $row;
+       }
+
+       return $rows;
+   }
+
+   function tambah($data){
+        global $conn;
+        $nrp  = htmlspecialchars($data["nrp"]);
+        $nama = htmlspecialchars($data["nama"]);
+        $email = htmlspecialchars($data["email"]);
+        $jurusan = htmlspecialchars($data["jurusan"]);
+        $gambar = htmlspecialchars($data["gambar"]);
+
+          // query insert data
+        $query = "INSERT INTO siswa VALUES ('','$nrp','$nama','$email','$jurusan','$gambar')";
+
+        mysqli_query($conn, $query);
+
+        return mysqli_affected_rows($conn);
+   }
+
+   function hapus($id){
+       global $conn;
+       $query = "DELETE FROM siswa WHERE id = $id";
+       mysqli_query($conn, $query);
+       return mysqli_affected_rows($conn);
+   }
+
+   function ubah($data){
+       global $conn;
+       $id = $data["id"];
+       $nrp  = htmlspecialchars($data["nrp"]);
+       $nama = htmlspecialchars($data["nama"]);
+       $email = htmlspecialchars($data["email"]);
+       $jurusan = htmlspecialchars($data["jurusan"]);
+       $gambar = htmlspecialchars($data["gambar"]);
+
+       $query = "UPDATE siswa SET 
+                nrp = '$nrp',
+                nama = '$nama',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+                WHERE id = $id
+            ";
+
+       mysqli_query($conn, $query);
+
+       return mysqli_affected_rows($conn);
+   }
+
+   function cari($keyword){
+        $query = "SELECT * FROM siswa WHERE nama LIKE '%$keyword%' OR
+        nrp LIKE '%$keyword%' OR
+        email LIKE '%$keyword%' OR
+        jurusan LIKE '%$keyword%' ";
+        return query($query);
+   }
+
+?>
---
+++ pertemuan12/ubah.php
@@ -0,0 +1,71 @@
+<?php
+require 'functions.php';
+
+// ambil data di url
+$id = $_GET["id"];
+// query data mahasiswa berdasarakan id
+$query = "SELECT * FROM siswa WHERE id = $id";
+$siswa = query($query)[0];
+
+
+// cek apakah tombol submit sudah di tekan?
+if (isset($_POST["submit"])) {
+    // cek apakah data berhasil di ubah atu tidak
+   if (ubah($_POST) > 0) {
+        echo "
+            <script>
+                alert('data berhasil di ubah');
+                document.location.href = 'index.php';
+            </script>
+        ";
+   }else{
+        echo "
+        <script>
+            alert('data gagal di ubah');
+            document.location.href = 'index.php';
+        </script>
+    ";
+   }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ubah Data</title>
+</head>
+<body>
+    <h2>Ubah Data Siswa</h2>
+    <form action="" method="POST">
+    <input type="hidden" name="id" value="<?= $siswa["id"]; ?>">
+        <ul>
+            <li>
+                <label for="nrp">NRP</label>
+                <input type="text" name="nrp" id="nrp" value="<?= $siswa["nrp"]; ?>" required>
+            </li>
+            <li>
+                <label for="nama">Nama</label>
+                <input type="text" name="nama" id="nama" value="<?= $siswa["nama"]; ?>" required>
+            </li>
+            <li>
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" required value="<?= $siswa["email"]; ?>">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan</label>
+                <input type="text" name="jurusan" id="jurusan" required value="<?= $siswa["jurusan"]; ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar</label>
+                <input type="text" name="gambar" id="gambar" required value="<?= $siswa["gambar"]; ?>">
+            </li>
+            <li>
+                <button type="submit" name="submit">Ubah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
